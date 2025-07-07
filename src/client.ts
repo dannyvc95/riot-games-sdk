b30fce2f359992e2650b4452e8591497cb7977bd
AlfonsoKrad <65353103+AlfonsoKrad@users.noreply.github.com>
2025-07-07 09:53:34 -0600

    Update get method to be use in regions
    Proposal of use for different regions when fetch is been called, this in order to be able to use all the APIs endpoints.

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import {Account} from './resources/account';
+import { Account } from './resources/account';
+import { Clash } from './resources/clash';
+
+type RiotRegion = 'americas' | 'la1';
 
 export class RiotGamesApiClient {
     public readonly account: Account;
+    public readonly clash: Clash;
 
     private readonly apiKey: string = process.env.RIOT_GAMES_API_KEY || '';
-    private readonly baseUrl: string = process.env.RIOT_GAMES_API_HOST || '';
+
+
+    private readonly regionUrls: Record<RiotRegion, string> = {
+        americas: process.env.RIOT_GAMES_API_AMERICAS_HOST || '',
+        la1: process.env.RIOT_GAMES_API_LA1_HOST || '',
+    };
 
     constructor() {
         this.account = new Account(this);
+        this.clash = new Clash(this);
     }
 
     /**
@@ -21,13 +31,15 @@ export class RiotGamesApiClient {
     * @returns A promise that resolves to the parsed JSON response of type T.
     * @throws If the response status is not OK.
     */
-    async get<T = unknown>(path: string): Promise<T> {
-        const response = await fetch(`${this.baseUrl}${path}`, {
-            headers: {'X-Riot-Token': this.apiKey},
+    async getFromRegion<T = unknown>(region: RiotRegion, path: string): Promise<T> {
+        const baseUrl = this.regionUrls[region];
+        const response = await fetch(`${baseUrl}${path}`, {
+            headers: { 'X-Riot-Token': this.apiKey },
         });
         if (!response.ok) {
             throw new Error(`Request failed: ${response.status}`);
         }
         return response.json();
     }
+
 }
--- src/resources/account.ts
+++ src/resources/account.ts
@@ -1,15 +1,15 @@
 /** https://developer.riotgames.com/apis#account-v1 */
-import {RiotGamesApiClient} from '../client';
-import {AccountDto} from '../types/AccountDto';
-import {AccountRegionDto} from '../types/AccountRegionDto';
+import { RiotGamesApiClient } from '../client';
+import { AccountDto } from '../types/AccountDto';
+import { AccountRegionDto } from '../types/AccountRegionDto';
 
 export class Account {
-    constructor(private client: RiotGamesApiClient) {}
+    constructor(private client: RiotGamesApiClient) { }
 
     /** /riot/account/v1/accounts/by-puuid/{puuid} */
     async getAccountByPuuid(puuid: string): Promise<AccountDto | null> {
         try {
-            return await this.client.get(`/riot/account/v1/accounts/by-puuid/${puuid}`);
+            return await this.client.getFromRegion('americas', `/riot/account/v1/accounts/by-puuid/${puuid}`);
         } catch (error) {
             console.error(error);
         }
@@ -19,7 +19,7 @@ export class Account {
     /** /riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine} */
     async getAccountByRiotId(gameName: string, tagLine: string): Promise<AccountDto | null> {
         try {
-            return await this.client.get(`/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`);
+            return await this.client.getFromRegion('americas', `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`);
         } catch (error) {
             console.error(error);
         }
@@ -30,7 +30,7 @@ export class Account {
     async getActiveRegionByPuuid(puuid: string): Promise<AccountRegionDto | null> {
         try {
             const game = 'lol';
-            return await this.client.get(`/riot/account/v1/region/by-game/${game}/by-puuid/${puuid}`);
+            return await this.client.getFromRegion('americas', `/riot/account/v1/region/by-game/${game}/by-puuid/${puuid}`);
         } catch (error) {
             console.error(error);
         }
