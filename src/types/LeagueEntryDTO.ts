897b60d5ec9b85bf668e9fed8369133ffc238b5e
AlfonsoKrad <65353103+AlfonsoKrad@users.noreply.github.com>
2025-07-09 09:10:44 -0600

    Create interfaces to be use in league fetch

---
+++ src/types/LeagueEntryDTO.ts
@@ -0,0 +1,73 @@
+import {MiniSeriesDTO} from './MiniSeriesDTO';
+
+export interface LeagueEntryDTO {
+  /**
+   * ID of the league the player belongs to.
+   */
+  leagueId: string;
+
+  /**
+   * Encrypted summoner ID of the player.
+   */
+  summonerId: string;
+
+  /**
+   * Encrypted PUUID of the player.
+   */
+  puuid: string;
+
+  /**
+   * Type of ranked queue.
+   */
+  queueType: string;
+
+  /**
+   * Tier of the player (e.g., IRON, BRONZE, SILVER, etc.).
+   */
+  tier: string;
+
+  /**
+   * Division within the tier (e.g., I, II, III, IV).
+   */
+  rank: string;
+
+  /**
+   * League Points (LP) the player has in this rank.
+   */
+  leaguePoints: number;
+
+  /**
+   * Number of wins.
+   */
+  wins: number;
+
+  /**
+   * Number of losses.
+   */
+  losses: number;
+
+  /**
+   * Whether the player is on a hot streak.
+   */
+  hotStreak: boolean;
+
+  /**
+   * Whether the player is a veteran in this league.
+   */
+  veteran: boolean;
+
+  /**
+   * Whether the player is considered fresh blood (new to the league).
+   */
+  freshBlood: boolean;
+
+  /**
+   * Whether the player is currently inactive.
+   */
+  inactive: boolean;
+
+  /**
+   * Mini series data if the player is in promotion series.
+   */
+  miniSeries?: MiniSeriesDTO;
+}
---
+++ src/types/MiniSeriesDTO.ts
@@ -0,0 +1,21 @@
+export interface MiniSeriesDTO {
+  /**
+   * Number of losses in the mini series.
+   */
+  losses: number;
+
+  /**
+   * Progress of the mini series as a string (e.g., "WLLW").
+   */
+  progress: string;
+
+  /**
+   * Number of wins required to complete the mini series.
+   */
+  target: number;
+
+  /**
+   * Number of wins in the mini series.
+   */
+  wins: number;
+}
